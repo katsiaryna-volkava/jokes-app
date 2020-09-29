6a8c2f726e58303118bf867a56d2a1bdb555b8eb
EkaterinaVolkova <e.volkova@syberry.com>
2020-09-30 01:53:39 +0300

    refactoring to use spring bean

--- src/main/java/com/evolkova/jokeapp/services/JokeServiceImpl.java
+++ src/main/java/com/evolkova/jokeapp/services/JokeServiceImpl.java
@@ -6,10 +6,10 @@ import org.springframework.stereotype.Service;
 @Service
 public class JokeServiceImpl implements JokeService {
 
-    private ChuckNorrisQuotes quotes;
+    private final ChuckNorrisQuotes quotes;
 
-    public JokeServiceImpl() {
-        this.quotes = quotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes quotes) {
+        this.quotes = quotes;
     }
 
     @Override
